e4d3ed2842ea12331c40d0545c8a241038d23983
shivam-cod2386 <shivamsureshdubey8206@gmail.com>
2026-03-03 11:51:41 +0530

    Calling using base pointer

---
+++ RuntimePolymorphismDemo.java
@@ -0,0 +1,33 @@
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class RuntimePolymorphismDemo {
+    public static void main(String[] args) {
+      
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+
+        
+        a1.sound();  
+        a2.sound();  
+    }
+}
